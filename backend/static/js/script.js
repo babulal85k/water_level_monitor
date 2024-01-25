6c61f66a6b34e9bfb64671a5e0f54476a9a550e8
Babu Lal Mandal <madhukarbabulal5@gmail.com>
2024-01-25 09:27:52 +0530

    updated

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     function fetchWaterLevel() {
-        $.get('http://127.0.0.1:5000/get_latest', function(data) {
+        $.get('/get_latest', function(data) {
             var waterLevel = parseInt(data.level);
             var maxHeight = 100; // Max height of the bar in pixels
             var heightPercentage = (waterLevel / maxHeight) * 100;
